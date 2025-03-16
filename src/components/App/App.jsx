58c81277e90419d88fe50217c6ba889fcbb3fea2
Khrystyna Pilip <tina.hristi@gmail.com>
2025-03-16 22:30:26 +0100

    Create file

---
+++ src/components/App/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import "../components/App.css";
+import LoadMoreBtn from "../../components/LoadMoreBtn";
+
+export default function App() {
+  return <div></div>;
+}
